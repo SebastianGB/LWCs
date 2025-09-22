8637e83c09fb7a5df221b26e3a4574ea7e6e9983
SebastianGB <sebasgarcia09@gmail.com>
2025-09-22 11:36:20 +0200

    Contact Create Form

--- force-app/main/default/lwc/contactCreate/contactCreate.js
+++ force-app/main/default/lwc/contactCreate/contactCreate.js
@@ -1,3 +1,38 @@
 import { LightningElement } from 'lwc';
+import { api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class ContactCreate extends LightningElement {}
+export default class ContactCreate extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+
+    handleSubmit(event) {
+        event.preventDefault();  
+        const fields = event.detail.fields;
+        fields.AccountId = this.recordId;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
+
+    handleSuccess(event) {
+        this.resetFields();
+        
+        const successToast = new ShowToastEvent({
+            title: 'Success',
+            message: 'Contact created',
+            variant: 'success',
+        });
+        this.dispatchEvent(successToast);
+    }
+
+    resetFields() {
+        const inputFields = this.template.querySelectorAll(
+            'lightning-input-field'
+        );
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();
+            });
+        }
+    }
+}
