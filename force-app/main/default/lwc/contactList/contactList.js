321a625dee7ff35e44ebabd3f6cfb0e8a271b292
SebastianGB <sebasgarcia09@gmail.com>
2025-09-22 13:59:16 +0200

    contactList LWC and ContactController

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
+import { api, wire } from 'lwc';
+import getContactsByAccountId from '@salesforce/apex/ContactController.getContactsByAccountId';
 
-export default class ContactList extends LightningElement {}
+const COLUMNS = [
+    {label: 'First Name', fieldName: 'FirstName'},
+    {label: 'Last Name', fieldName: 'LastName'},
+    {label: 'Email', fieldName: 'Email'},
+    {label: 'Phone', fieldName: 'Phone'}
+];
+
+
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @api recordId;
+
+    @wire(getContactsByAccountId, {accountId: '$recordId'})
+    contacts;
+}
